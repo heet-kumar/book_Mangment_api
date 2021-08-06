f160d2e958d69b770167f8583a55fdbf6b288219
heet-kumar <heetkumarpatel@gmail.com>
2021-08-07 00:15:01 +0530

    Database setup done

--- database/index.js
+++ database/index.js
@@ -1,7 +1,50 @@
 let books = [
     {
-        ISBN : "12345ONE",
-        Title : "Getting started with Mern",
-        
+        ISBN: "12345ONE",
+        Title: "Getting started with Mern",
+        authors: [1,2,3],
+        language: "en",
+        pubDate: "2021-07-07",
+        numOfPage: 225,
+        category: ["fiction","programming","tech","web dev"],
+        publication: 1      
+    },
+    {
+        ISBN: "12345TWO",
+        Title: "Getting started with DevOps",
+        authors: [2],
+        language: "en",
+        pubDate: "2021-07-07",
+        numOfPage: 225,
+        category: ["Dev Ops","tech","web dev"],
+        publication: 1      
     }
 ];
+
+let authors = [
+    {
+        id: 1,
+        name: "Heet kumar Kothadiya",
+        books: ["12345ONE"]
+    },
+    {
+        id: 2,
+        name: "Saloni",
+        books:["12345ONE","12345TWO"]
+    },
+    {
+        id: 3,
+        name: "Heet Kothadiya",
+        books: ["12345ONE"]
+    }
+];
+
+let publications = [
+    {
+        id: 1,
+        name: "Hector",
+        books: ["12345ONE","12345TWO"]
+    }
+];
+
+module.exports = {books,authors,publications};
